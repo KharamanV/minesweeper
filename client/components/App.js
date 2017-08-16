67259e5f6df55f5a3040570dff8737a54fab92ca
Vladimir <vladimir@code-care.pro>
2017-08-16 19:26:46 +0300

    Remove non-stable components

--- client/components/App.js
+++ client/components/App.js
@@ -1,27 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import Board from './Board';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      minesCount: null,
-      size: null,
-    };
-  }
-
-  componentWillMount() {
-    // 5979ea24c2723f6a56023867
-  }
-
   render() {
     return (
       <div className="app">
         Hello from React!
-
-        <Board {...this.state} />
       </div>
     );
   }
