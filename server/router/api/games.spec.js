fe2f33254493f819f908a52cde7372c8a778f7b0
Vladimir <kharaman.v@gmail.com>
2017-08-24 20:10:23 +0300

    Add test packages

---
+++ server/router/api/games.spec.js
@@ -0,0 +1,9 @@
+const { assert } = require('../../services/test');
+
+const a = 1;
+
+describe('Your mom', () => {
+  it('should fuck yourself', () => {
+    assert.equal(a, 1, 'MMMMM');
+  });
+});
